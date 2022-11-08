f12b48fa5dac0f8fc8e4511662ce336b6e7c8c57
olejka <114016068+Olegggg12@users.noreply.github.com>
2022-11-08 15:09:35 +0300

    Create create_db.py

---
+++ db/create_db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_NAME = "user_data.sqlite"
+
+engine = create_engine(f"sqlite:///{DATABASE_NAME}")
+session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+def create_db():
+    Base.metadata.create_all(engine)
